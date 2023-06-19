b7ee3513bf6d24e23c00bc30e8801d342f5b4c14
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2023-06-20 02:16:10 +0400

    Add logging widget

--- src/ui/main_window.cpp
+++ src/ui/main_window.cpp
@@ -1,13 +1,29 @@
+#include <QDockWidget>
+
 #include "main_window.hpp"
 
+#include "qspdlog/qspdlog.hpp"
 #include "view.hpp"
+#include <spdlog/sinks/sink.h>
+#include <spdlog/spdlog.h>
 
 MainWindow::MainWindow(QWidget* parent)
-	: QMainWindow(parent)
+    : QMainWindow(parent)
 {
-	setCentralWidget(new View(this));
+    View* view = new View(this);
+    view->setFixedSize(800, 600);
+    setCentralWidget(view);
+
+    QDockWidget* loggerDockWidget = new QDockWidget("Logger", this);
+    loggerDockWidget->setAllowedAreas(Qt::BottomDockWidgetArea);
+
+    QSpdLog* qspdlog = new QSpdLog(loggerDockWidget);
+    auto& registry = spdlog::details::registry::instance();
+    registry.apply_all([ qspdlog ](std::shared_ptr<spdlog::logger> logger)
+                       { logger->sinks().push_back(qspdlog->sink()); });
+
+    loggerDockWidget->setWidget(qspdlog);
+    addDockWidget(Qt::BottomDockWidgetArea, loggerDockWidget);
 }
 
-MainWindow::~MainWindow()
-{
-}
+MainWindow::~MainWindow() { }
